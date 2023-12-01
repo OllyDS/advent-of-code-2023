615f88c58e3c5fa40b15c4fac1084150852682b2
OllyDS <0lly@protonmail.com>
2023-12-01 17:26:38 +0000

    init repo

---
+++ src/day_01.rs
@@ -0,0 +1,7 @@
+use crate::utils::read_file;
+
+pub fn day_one_calc() {
+    let contents = read_file("./src/files/day_01.txt".to_string());
+
+    println!("{:?}", contents);
+}
---
+++ src/utils.rs
@@ -0,0 +1,4 @@
+/// takes a path for the location of the text file and converts to a String.
+pub fn read_file(path: String) -> String {
+    fs::read_to_string(path).expect("Should have been able to read the file")
+}
